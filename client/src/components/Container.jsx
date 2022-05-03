07e1214627feb7296d045fd809a21168fc59a32e
Edgar Carrillo <ecarrillo046@gmail.com>
2022-05-03 10:52:39 -0700

    refactor: add no padding option

--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 
 const StyledContainer = styled.div`
   position: relative;
-  max-width: 2000px;
+  max-width: 1800px;
   margin: auto;
   padding: 0 10px;
+  ${({ noPadding }) => noPadding && 'padding: 0;'}
 `;
 
 export default StyledContainer;
