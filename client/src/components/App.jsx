43e01f289674512566d7e9c2efff301a7ef6463b
Edgar Carrillo <ecarrillo046@gmail.com>
2022-05-03 09:36:54 -0700

    feat: add NavBar

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+// Components
+import StyledNavBar from './NavBar/NavBar';
+
 function App({ className }) {
   return (
     <div className={className}>
-      Hello!
+      <StyledNavBar />
     </div>
   );
 }
 
 const StyledApp = styled(App)`
-  max-width: 2000px;
+  width: 100%;
   margin: auto;
 `;
 
