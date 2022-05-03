ba390c095db624e7a293a39cf4e85cbbad9c3618
Edgar Carrillo <ecarrillo046@gmail.com>
2022-05-03 09:36:32 -0700

    feat: add master container

---
+++ client/src/components/Container.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  position: relative;
+  max-width: 2000px;
+  margin: auto;
+  padding: 0 10px;
+`;
+
+export default StyledContainer;
