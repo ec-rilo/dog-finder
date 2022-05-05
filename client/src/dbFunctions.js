4b3c03659abb02e8b2c0a3ae6c4fafbdd908da28
Edgar Carrillo <ecarrillo046@gmail.com>
2022-05-05 12:06:16 -0700

    feat: allow start points to be accessible through .env

--- client/src/dbFunctions.js
+++ client/src/dbFunctions.js
@@ -3,19 +3,19 @@ import axios from 'axios';
 const dbFunctions = {
 
   addDog: (id, dogName) => new Promise((resolve, reject) => {
-    axios.post(`/user/dogs?userId=${id}&dogName=${dogName}`)
+    axios.post(`${process.env.db_host}/user/dogs?userId=${id}&dogName=${dogName}`)
       .then((data) => resolve(data))
       .catch((err) => reject(err));
   }),
 
   getDogs: (id) => new Promise((resolve, reject) => {
-    axios.get(`/user/dogs?userId=${id}`)
+    axios.get(`${process.env.db_host}/user/dogs?userId=${id}`)
       .then((response) => resolve(response.data))
       .catch((err) => reject(err));
   }),
 
   removeDog: (id, dogName) => new Promise((resolve, reject) => {
-    axios.delete(`/user/dogs?userId=${id}&dogName=${dogName}`)
+    axios.delete(`${process.env.db_host}/user/dogs?userId=${id}&dogName=${dogName}`)
       .then((response) => resolve(response))
       .catch((err) => reject(err));
   }),
