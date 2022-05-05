0c59949f45a1c4ab221e79d7cc43be31f2faece9
Edgar Carrillo <ecarrillo046@gmail.com>
2022-05-05 12:03:55 -0700

    feat: install and implement dotenv-webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Dotenv = require('dotenv-webpack');
 const path = require('path');
 
 module.exports = {
@@ -49,5 +50,6 @@ module.exports = {
       template: path.resolve(__dirname, './client/src/index.html'),
     }),
     new MiniCssExtractPlugin(),
+    new Dotenv(),
   ],
 };
